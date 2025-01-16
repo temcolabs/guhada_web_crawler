579e3e001cd30106a625c5fb67a475fb84769469
GUHADA <jaejin.yoon@temco.io>
2025-01-16 17:32:28 +0900

    test

--- windowStart.cmd
+++ windowStart.cmd
@@ -2,18 +2,19 @@
 cd /d "%~dp0"
 
 REM 3000번 포트 사용 여부 확인 및 종료 (강제 종료)
-@echo off
 echo Checking for processes using port 3000...
 
-REM 3000번 포트를 사용하는 프로세스 PID 찾기
+set "PORT_IN_USE=0"
 for /f "tokens=5" %%a in ('netstat -ano ^| findstr :3000') do (
     echo Port 3000 is in use. Killing process (PID: %%a)...
     taskkill /PID %%a /F
     echo Process %%a has been terminated.
-    goto :END
+    set "PORT_IN_USE=1"
 )
 
-echo Port 3000 is not in use. Skipping termination.
+if %PORT_IN_USE%==0 (
+    echo Port 3000 is not in use. Proceeding...
+)
 
 REM npm 경로 확인
 where npm >nul 2>&1
@@ -26,7 +27,6 @@ if errorlevel 1 (
 REM npm install 실행
 echo "Installing dependencies..."
 call npm run init
-
 
 REM 새로운 터미널 창에서 npm run dev 실행
 echo "Starting npm run dev..."
