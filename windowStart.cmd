68d27157f05cbd2c9e4293cddd37de0898e42b92
GUHADA <jaejin.yoon@temco.io>
2025-01-16 12:26:55 +0900

    test

--- windowStart.cmd
+++ windowStart.cmd
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
+REM npm 경로 확인
 where npm >nul 2>&1
 if errorlevel 1 (
     echo "npm 명령어를 찾을 수 없습니다. Node.js가 설치되어 있는지 확인하세요."
@@ -12,8 +13,6 @@ REM npm install 실행
 echo "Installing dependencies..."
 call npm run init
 
-REM npm run dev 실행
+REM 새로운 터미널 창에서 npm run dev 실행
 echo "Starting npm run dev..."
-call npm run dev
-
-pause
+start cmd /k "npm run devWindow"
