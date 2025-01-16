fd01d2d20035bb884bf080d3c95f55b0d69368cd
GUHADA <jaejin.yoon@temco.io>
2025-01-16 12:09:32 +0900

    test

--- windowStart.bat
+++ windowStart.bat
@@ -8,12 +8,10 @@ REM 현재 디렉토리로 이동
 cd /d "%~dp0"
 
 REM npm 설치 확인
-npm -v || echo "npm 명령어를 찾을 수 없습니다."
 
 REM npm install 실행
+npm -v || echo "npm 명령어를 찾을 수 없습니다."
 npm install -f || echo "npm install 실패"
-
-REM npm run dev 실행
 call npm run dev || echo "npm run dev 실패"
 
 REM 종료 방지
