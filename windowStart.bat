6b91033c6496938e09093a1ac0cee6b242bc4f50
GUHADA <jaejin.yoon@temco.io>
2025-01-16 11:50:29 +0900

    test

--- windowStart.bat
+++ windowStart.bat
@@ -1,8 +1,14 @@
 @echo off
-REM 현재 파일의 디렉토리로 이동
+REM 현재 디렉토리로 이동
 cd /d "%~dp0"
 
-REM start.sh 실행
-@REM bash start.sh
+REM npm run init 실행
+echo Running "npm run init"...
 npm run init
+
+REM npm run dev 실행
+echo Running "npm run dev"...
 npm run dev
+
+REM 종료를 방지하기 위해 대기
+pause
