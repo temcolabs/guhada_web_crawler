bc9c811e9b286df59eb5cfc970fd41ad139a94ac
GUHADA <jaejin.yoon@temco.io>
2025-01-17 16:44:44 +0900

    tset

--- src/template/main/UploadFile.tsx
+++ src/template/main/UploadFile.tsx
@@ -195,7 +195,7 @@ const UploadFile = () => {
           크로니움 {isChromium ? "on" : "off"}
         </button>
       </form>
-      <div className="text-end font-bold">0.992ver</div>
+      <div className="text-end font-bold">0.993ver</div>
     </div>
   );
 };
