77064299f85b6c83eb352f856732c6564ebe50ee
GUHADA <jaejin.yoon@temco.io>
2025-01-17 16:11:22 +0900

    tset

--- windowStart.cmd
+++ windowStart.cmd
@@ -176,7 +176,7 @@ REM ===============================
 :StartServer
 echo "Starting Next.js server..."
 @REM start cmd /k "npm run dev"
-start "Next.js Server" cmd /c "npm run start > server.log 2>&1"
+powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c npm run start > server.log 2>&1' -NoNewWindow"
 
 REM Wait for the server to become available
 echo Waiting for server at http://localhost:3000...
